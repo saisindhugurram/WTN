f688ea3da00a9fb9735c244cf5c0553bcd9c5b39
Gurram Sai Sindhu <Gurram Sai Sindhu@DESKTOP-17VJU5S>
2020-06-14 21:41:55 +0530

    pattern exercises

---
+++ Milestone1/src/com/wipro/patterns/Pt1.java
@@ -0,0 +1,23 @@
+package com.wipro.patterns;
+
+//****
+//****
+//****
+//****
+
+public class Pt1
+{
+	public static void main(String args[]) {
+		int i,j;
+		int n=Integer.parseInt(args[0]);
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				System.out.print("*");
+			}System.out.println("");
+		}
+	}
+
+}
+  
