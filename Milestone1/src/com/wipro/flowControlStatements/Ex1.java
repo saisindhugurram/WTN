048ab3537a348b92c1740b9b5c9e4d230ec9d840
Gurram Sai Sindhu <Gurram Sai Sindhu@DESKTOP-17VJU5S>
2020-06-14 21:13:06 +0530

    Ex 1 flow control

---
+++ Milestone1/src/com/wipro/flowControlStatements/Ex1.java
@@ -0,0 +1,18 @@
+package com.wipro.flowControlStatements;
+
+public class Ex1 {
+	public static void main(String args[]) {
+		int a=Integer.parseInt(args[0]);
+		if(a>0) {
+			System.out.println("Positive");
+		}
+		else if(a<0) {
+			System.out.println("Negative");
+		}
+		else {
+			System.out.println("Zero");
+		}
+		
+	}
+
+}
