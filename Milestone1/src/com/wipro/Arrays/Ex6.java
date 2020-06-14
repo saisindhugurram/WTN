37754df2439da1108a79fed160d5c646a79470bc
Gurram Sai Sindhu <Gurram Sai Sindhu@DESKTOP-17VJU5S>
2020-06-14 21:34:43 +0530

    Array exercises

---
+++ Milestone1/src/com/wipro/Arrays/Ex6.java
@@ -0,0 +1,18 @@
+package com.wipro.Arrays;
+
+import java.util.Arrays;
+
+public class Ex6 {
+	public static void main(String args[]) {
+		int arr[]=new int[args.length];
+		int i;
+		for(i=0;i<args.length;i++) {
+			arr[i]=Integer.parseInt(args[i]);
+			
+		}
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+		
+	}
+
+}
